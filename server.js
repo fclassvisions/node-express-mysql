faac3bd04d0bc0b1c54d2195e89c07f13fada3bf
fclassvisions <fclassvisions@gmail.com>
2022-04-18 19:45:04 -0400

    Added ticket CRUD actions

--- server.js
+++ server.js
@@ -11,8 +11,70 @@ const pool = mysql.createPool({
   connectionLimit: 10,
 });
 
-app.get("/", (req, res) => {
-  pool.query("select * from test", (error, result) => {
+app.use(express.json());
+
+app.post("/ticket", (req, res) => {
+  const ticket = req.body;
+  pool.query(
+    "insert into ticket (summary,priority,status) value (?,?,?)",
+    [ticket.summary, ticket.priority, ticket.status],
+    (error, result) => {
+      if (error) {
+        console.error(error);
+        res.send(error);
+        return;
+      }
+
+      res.send({ id: result.insertId, ...ticket });
+    }
+  );
+});
+
+app.put("/ticket", (req, res) => {
+  const ticket = req.body;
+  pool.query(
+    "update ticket set ? where id = ?",
+    [ticket, ticket.id],
+    (error, result) => {
+      if (error) {
+        console.error(error);
+        res.send(error);
+        return;
+      }
+
+      res.send(ticket);
+    }
+  );
+});
+
+app.delete("/ticket/:id", (req, res) => {
+  const id = req.params.id;
+  pool.query("delete from ticket where id = ?", [id], (error, result) => {
+    if (error) {
+      console.error(error);
+      res.send(error);
+      return;
+    }
+
+    res.send("Success");
+  });
+});
+
+app.get("/all", (req, res) => {
+  pool.query("select * from ticket", (error, result) => {
+    if (error) {
+      console.error(error);
+      res.send(error);
+      return;
+    }
+
+    res.send(result);
+  });
+});
+
+app.get("/ticket/:id", (req, res) => {
+  const id = req.params.id;
+  pool.query("select * from ticket where id = ?", [id], (error, result) => {
     if (error) {
       console.error(error);
       res.send(error);
